e8c71f4c1940891ca8865189d041aa9aa816efa3
Big Dog Dean <diazdean21@gmail.com>
2022-04-12 16:31:10 -0700

    Completed Assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+    attr_accessor :knowledge
+
+    def Initalize
+        @knowledge = []
+    end
+    
+    def learn(string)
+        @knowledge = "Ruby framework Rails gem bundle update"
+    end
+
+    def knowledge
+        @knowledge = "Javascript Ember Elixir knowledge"
+    end
 
 end
